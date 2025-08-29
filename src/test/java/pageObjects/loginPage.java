f8979cc72c1c20ab6533e903590d55e61e6680aa
reyoctavially <reynaldi.vendor.pasti@pegadaian.co.id>
2025-08-29 09:24:22 +0700

    Initial commit

---
+++ src/test/java/pageObjects/loginPage.java
@@ -0,0 +1,32 @@
+package pageObjects;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class loginPage {
+    WebDriver driver;
+
+    public loginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    By usernameField = By.id("username");
+    By passwordField = By.id("password");
+    By loginButton   = By.cssSelector("button[type='submit']");
+    By flashMessage  = By.id("flash");
+
+    public void enterUsername(String username) {
+        driver.findElement(usernameField).sendKeys(username);
+    }
+
+    public void enterPassword(String password) {
+        driver.findElement(passwordField).sendKeys(password);
+    }
+
+    public void clickLogin() {
+        driver.findElement(loginButton).click();
+    }
+
+    public String getFlashMessage() {
+        return driver.findElement(flashMessage).getText();
+    }
+}
---
+++ src/test/java/stepDefinitions/loginSteps.java
@@ -0,0 +1,38 @@
+package stepDefinitions;
+
+import io.cucumber.java.PendingException;
+import io.cucumber.java.en.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pageObjects.loginPage;
+
+public class loginSteps {
+    WebDriver driver;
+    loginPage loginPage;
+
+    @Given("user is on login page")
+    public void userIsOnLoginPage() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://the-internet.herokuapp.com/login");
+        loginPage = new loginPage(driver);
+    }
+
+    @When("user enters valid username and password")
+    public void userEntersValidUsernameAndPassword() {
+        loginPage.enterUsername("tomsmith");
+        loginPage.enterPassword("SuperSecretPassword!");
+        loginPage.clickLogin();
+    }
+
+    @Then("user should be redirected to homepage")
+    public void userShouldBeRedirectedToHomepage() {
+        String message = loginPage.getFlashMessage();
+        if (message.contains("You logged into a secure area!")) {
+            System.out.println("✅ Login sukses!");
+        } else {
+            System.out.println("❌ Login gagal!");
+        }
+        driver.quit();
+    }
+}
